0d8b6d9994e026c8042aafb31e4771e0941a494f
Paul Swan <pswan@cocoondata.com>
2022-08-16 16:42:06 +1000

    Bootstrap setup

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,9 +2,16 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  template: `<h1>Hello {{ name }}!</h1>`,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `,
+  ],
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent {
+  @Input()
+  name!: string;
 }
