89a082191170b0520a0fef16cb049577d71cd289
Paul Swan <pswan@cocoondata.com>
2022-08-17 15:33:14 +1000

    Playing with colour palettes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,98 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ColorsComponent } from './colors/colors.component';
+// import { AddressFormComponent } from './address-form/address-form.component';
+// import { ButtonsComponent } from './buttons/buttons.component';
+// import { CardsComponent } from './cards/cards.component';
+// import { CheckboxRadioComponent } from './checkbox-radio/checkbox-radio.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { DragDropComponent } from './drag-drop/drag-drop.component';
+// import { HomeComponent } from './home/home.component';
+// import { InputsComponent } from './inputs/inputs.component';
+// import { OtherComponent } from './other/other.component';
+// import { ProgressComponent } from './progress/progress.component';
+// import { SlidersComponent } from './sliders/sliders.component';
+// import { TabStepperComponent } from './tab-stepper/tab-stepper.component';
+// import { TableComponent } from './table/table.component';
+// import { TreeComponent } from './tree/tree.component';
+// import { TypographyComponent } from './typography/typography.component';
+
+const routes: Routes = [
+  {
+    path: 'colors',
+    component: ColorsComponent,
+  },
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'home',
+  // },
+  // {
+  //   path: 'home',
+  //   component: HomeComponent,
+  // },
+  // {
+  //   path: 'buttons',
+  //   component: ButtonsComponent,
+  // },
+  // {
+  //   path: 'typography',
+  //   component: TypographyComponent,
+  // },
+  // {
+  //   path: 'checkbox-radio',
+  //   component: CheckboxRadioComponent,
+  // },
+  // {
+  //   path: 'inputs',
+  //   component: InputsComponent,
+  // },
+  // {
+  //   path: 'dashboard',
+  //   component: DashboardComponent,
+  // },
+  // {
+  //   path: 'table',
+  //   component: TableComponent,
+  // },
+  // {
+  //   path: 'address-form',
+  //   component: AddressFormComponent,
+  // },
+  // {
+  //   path: 'tree',
+  //   component: TreeComponent,
+  // },
+  // {
+  //   path: 'drag-drop',
+  //   component: DragDropComponent,
+  // },
+  // {
+  //   path: 'cards',
+  //   component: CardsComponent,
+  // },
+  // {
+  //   path: 'progress',
+  //   component: ProgressComponent,
+  // },
+  // {
+  //   path: 'tab-stepper',
+  //   component: TabStepperComponent,
+  // },
+  // {
+  //   path: 'sliders',
+  //   component: SlidersComponent,
+  // },
+  // {
+  //   path: 'other',
+  //   component: OtherComponent,
+  // },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HelloComponent } from './hello.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ColorsComponent } from './colors/colors.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, NgbModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    AppRoutingModule,
+    //
+  ],
+  declarations: [AppComponent, HelloComponent, ColorsComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
