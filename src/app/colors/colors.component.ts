6b71e869cc38af0a2995e21427af50a0ac830ecc
paulswan <paul.swan@outlook.com.au>
2022-09-21 10:26:58 +1000

    Fixed colour indicators

--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ColorsComponent implements OnInit {
   tintShadeColors = ['tint-shade-purple-900', 'tint-shade-green-200', 'tint-shade-yellow-200'];
   lightenDarkenColors = ['lighten-darken-purple-800', 'lighten-darken-green-200', 'lighten-darken-yellow-200'];
-  angularMaterialColors = ['angular-material-purple-900', 'angular-material-green-200', 'angular-material-yellow-300'];
+  materialDesign2Colors = ['md2-purple-900', 'md2-green-200', 'md2-yellow-300'];
 
   constructor() {}
 
