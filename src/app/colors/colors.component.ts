21164dd0b9f61fe8203ab2de8df99a8a31a02aad
Paul Swan <paul.swan@outlook.com.au>
2022-09-21 18:09:22 +1000

    Added grey palettes

--- src/app/colors/colors.component.ts
+++ src/app/colors/colors.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./colors.component.scss'],
 })
 export class ColorsComponent implements OnInit {
-  tintShadeColors = ['tint-shade-purple-900', 'tint-shade-green-200', 'tint-shade-yellow-200'];
-  lightenDarkenColors = ['lighten-darken-purple-800', 'lighten-darken-green-200', 'lighten-darken-yellow-200'];
-  materialDesign2Colors = ['md2-purple-900', 'md2-green-200', 'md2-yellow-300'];
+  tintShadeColors = ['tint-shade-purple-900', 'tint-shade-green-200', 'tint-shade-yellow-200', 'tint-shade-grey-800'];
+  lightenDarkenColors = [
+    'lighten-darken-purple-800',
+    'lighten-darken-green-200',
+    'lighten-darken-yellow-200',
+    'lighten-darken-grey-800',
+  ];
+  materialDesign2Colors = ['md2-purple-900', 'md2-green-200', 'md2-yellow-300', 'md2-grey-800'];
 
   constructor() {}
 
